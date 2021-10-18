81180cd545f6af41bb5c1d57a26e4d90e395776d
Tony <t27302013@correo.unimet.edu.ve>
2021-10-18 14:26:38 -0400

    viendo como combinar el back con el front
    fdfdfdfdf

--- frontend/src/screens/Estadisticas/Estadisticas.js
+++ frontend/src/screens/Estadisticas/Estadisticas.js
@@ -1,61 +1,86 @@
-import React, { Component} from "react";
+import React, { useEffect} from "react";
 import MainScreen from "../../components/mainscreen/MainScreen";
 import EstadisticaColumna from "../../components/Estadisticas/EstadisticasColumna";
 
+import { useDispatch, useSelector } from "react-redux";
+import { listEntries } from "../../actions/entryActions";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
 
+const Estadisticas = ({ history }) => {
+    const dispatch = useDispatch();
 
-class Estadisticas extends Component {
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+    const entryList = useSelector((state) => state.entryList);
+    const { loading, error, entries } = entryList;
 
-    constructor(){
-
-        super();
-        this.state = {
-
-            chartData:{}
+    useEffect(() => {
+        dispatch(listEntries());
+        if (!userInfo) {
+           history.push("/");
         }
-    }
+    }, [dispatch, history, userInfo]);
 
-    componentWillMount(){
+    return(
+        <MainScreen title="Estadisticas">
+                
+        </MainScreen>
+    );
+}
 
-        this.getChartData();
-    }
+// class Estadisticas extends Component {
 
-    getChartData(){
+//     constructor(){
 
-        this.setState({
+//         super();
+//         this.state = {
 
-            chartData:{
-                labels:["Muy Feliz","Feliz","Normal","Triste","Muy Triste"],
-                datasets:[{
-                    label:"Numero de entradas",
-                    data:[
-                        30,
-                        15,
-                        60,
-                        30,
-                        30
-                    ],
-                    backgroundColor:[
+//             chartData:{}
+//         }
+//     }
 
-                        "rgba(255,0,0,0.6)",
-                        "rgba(0,255,0,0.6)",
-                        "rgba(0,0,255,0.6)",
-                        "rgba(255,255,0,0.6)",
-                        "rgba(0,255,255,0.6)"
-                    ]
-                }]
-            }
-        });
-    }
+//     componentWillMount(){
 
-    render(){
+//         this.getChartData();
+//     }
 
-        return(
-            <MainScreen title="Estadisticas">
-                <EstadisticaColumna chartData ={this.state.chartData}/>
-            </MainScreen>
-        );
-    } 
-};
+//     getChartData(){
+
+//         this.setState({
+
+//             chartData:{
+//                 labels:["Muy Feliz","Feliz","Normal","Triste","Muy Triste"],
+//                 datasets:[{
+//                     label:"Numero de entradas",
+//                     data:[
+//                         30,
+//                         15,
+//                         60,
+//                         30,
+//                         30
+//                     ],
+//                     backgroundColor:[
+
+//                         "rgba(255,0,0,0.6)",
+//                         "rgba(0,255,0,0.6)",
+//                         "rgba(0,0,255,0.6)",
+//                         "rgba(255,255,0,0.6)",
+//                         "rgba(0,255,255,0.6)"
+//                     ]
+//                 }]
+//             }
+//         });
+//     }
+
+//     render(){
+
+//         return(
+//             <MainScreen title="Estadisticas">
+//                 <EstadisticaColumna chartData ={this.state.chartData}/>
+//             </MainScreen>
+//         );
+//     } 
+// };
  
 export default Estadisticas;
