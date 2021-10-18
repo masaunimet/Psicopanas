f5f13984c91717391d6b54e759a8d8baa9a44a6f
Dinalba Navas <dinalba.navas@correo.unimet.edu.ve>
2021-10-18 19:35:40 -0400

    cambios con la nueva BD

--- frontend/src/screens/Estadisticas/Estadisticas.js
+++ frontend/src/screens/Estadisticas/Estadisticas.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import MainScreen from "../../components/mainscreen/MainScreen";
 import EstadisticaColumna from "../../components/Estadisticas/EstadisticasColumna";
 
@@ -9,83 +9,90 @@ import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 
 const Estadisticas = ({ history }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const userLogin = useSelector((state) => state.userLogin);
-    const { userInfo } = userLogin;
-    const entryList = useSelector((state) => state.entryList);
-    const { loading, error, entries } = entryList;
-    const emotionList = useSelector((state) => state.emotionList);
-    const { emotions } = emotionList;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const entryList = useSelector((state) => state.entryList);
+  const { loading, error, entries } = entryList;
+  const emotionList = useSelector((state) => state.emotionList);
+  const { emotions } = emotionList;
 
-    let muyFeliz =0;
-    let feliz =0;
-    let Normal =0;
-    let triste =0;
-    let muyTriste =0;
+  //Acumuladores
+  let muyFeliz = 0;
+  let feliz = 0;
+  let Normal = 0;
+  let triste = 0;
+  let muyTriste = 0;
 
-    const idemotions = emotions?.map((emotion)=>emotion._id);
+  const idemotions = emotions?.map((emotion) => emotion.icon);
 
-    const Logic =entry=>{
+  const Logic = (entry) => {
+    //console.log(idemotions);
+    //console.log(entry);
 
-        console.log(idemotions);
-        console.log(entry);
+    if (entry.emotion === idemotions[0]) muyFeliz++;
+    else if (entry.emotion === idemotions[1]) feliz++;
+    else if (entry.emotion === idemotions[2]) Normal++;
+    else if (entry.emotion === idemotions[3]) triste++;
+    else if (entry.emotion === idemotions[4]) muyTriste++;
+  };
 
-        if(entry._id === idemotions[0])
-            muyFeliz++;
-        else if(entry._id === idemotions[1])
-            feliz++;
-        else if(entry._id === idemotions[2])
-            Normal++;
-        else if(entry._id === idemotions[3])
-            triste++;
-        else if(entry._id === idemotions[4])
-            muyTriste++;
-    };
+  useEffect(() => {
+    dispatch(listEmotions());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(listEmotions());
-      }, [dispatch]);
+  useEffect(() => {
+    dispatch(listEntries());
+    if (!userInfo) {
+      history.push("/");
+    }
+  }, [dispatch, history, userInfo]);
 
-    useEffect(() => {
-        dispatch(listEntries());
-        if (!userInfo) {
-           history.push("/");
-        }
-        entries?.map((entry)=>(Logic(entry)));
-    }, [dispatch, history, userInfo]);  
-
-    //console.log("MuyFeliz:"+muyFeliz+", Feliz:"+feliz+", Normal:"+Normal+", Triste:"+triste+", MuyTriste:"+muyTriste);
-
-    let data ={
-        labels:["Muy Feliz","Feliz","Normal","Triste","Muy Triste"],
-        datasets:[{
-            label:"Numero de entradas",
-            data:[
-                {muyFeliz},
-                {feliz},
-                {Normal},
-                {triste},
-                {muyTriste}
-            ],
-            backgroundColor:[
-        
-                "rgba(255,0,0,0.6)",
-                "rgba(0,255,0,0.6)",
-                "rgba(0,0,255,0.6)",
-                "rgba(255,255,0,0.6)",
-                "rgba(0,255,255,0.6)"
-                ]  
-            }]
-        };
-
-    return(
-        <MainScreen title="Estadisticas">
-            {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-            {loading && <Loading />}
-            <EstadisticaColumna chartData ={data} />
-        </MainScreen>
+  const calculate = () => {
+    entries?.map((entry) => Logic(entry));
+    console.log(
+      "MuyFeliz:" +
+        muyFeliz +
+        ", Feliz:" +
+        feliz +
+        ", Normal:" +
+        Normal +
+        ", Triste:" +
+        triste +
+        ", MuyTriste:" +
+        muyTriste
     );
-}
- 
+  };
+
+  calculate();
+
+  //console.log("MuyFeliz:"+muyFeliz+", Feliz:"+feliz+", Normal:"+Normal+", Triste:"+triste+", MuyTriste:"+muyTriste);
+
+  let data = {
+    labels: ["Muy Feliz", "Feliz", "Normal", "Triste", "Muy Triste"],
+    datasets: [
+      {
+        label: "Numero de entradas",
+        data: [{ muyFeliz }, { feliz }, { Normal }, { triste }, { muyTriste }],
+        backgroundColor: [
+          "rgba(255,0,0,0.6)",
+          "rgba(0,255,0,0.6)",
+          "rgba(0,0,255,0.6)",
+          "rgba(255,255,0,0.6)",
+          "rgba(0,255,255,0.6)",
+        ],
+      },
+    ],
+  };
+
+  return (
+    <MainScreen title="Estadisticas">
+      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+      {loading && <Loading />}
+      <EstadisticaColumna chartData={data} />
+    </MainScreen>
+  );
+};
+
 export default Estadisticas;
