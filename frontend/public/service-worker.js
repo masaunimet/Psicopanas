9c2e916beb39925ac21e85f3623cac6084eef85e
Dinalba Navas <dinalba.navas@correo.unimet.edu.ve>
2021-11-13 16:24:41 -0400

    PWA2

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,8 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js");
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
+);
 
-workbox.setConfig({debug: false});
+workbox.setConfig({ debug: false });
 
 // workbox.routing.registerRoute(
 //     ({ request }) => request.mode === 'navigate',
@@ -8,24 +10,24 @@ workbox.setConfig({debug: false});
 // );
 
 // workbox.routing.registerRoute(
-//     ({ request }) => 
+//     ({ request }) =>
 //     request.destination === 'style' || request.destination === 'script' ||request.destination === 'worker',
 //     new workbox.strategies.StaleWhileRevalidate()
 // );
 
 workbox.routing.registerRoute(
-    ({request})=>request.destination ==='image',
-    new workbox.strategies.CacheFirst()
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst()
 );
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-// Catch routing errors, like if the user is offline
-workbox.routing.setCatchHandler(async ({ event }) => {
-  // Return the precached offline page if a document is being requested
-  if (event.request.destination === 'document') {
-    return matchPrecache('/offline.html');
-  }
+// // Catch routing errors, like if the user is offline
+// workbox.routing.setCatchHandler(async ({ event }) => {
+//   // Return the precached offline page if a document is being requested
+//   if (event.request.destination === 'document') {
+//     return matchPrecache('/offline.html');
+//   }
 
-  return Response.error();
-});
+//   return Response.error();
+// });
