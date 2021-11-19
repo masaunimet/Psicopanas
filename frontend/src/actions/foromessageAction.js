b017446409d87ff674158bf36e182fee7e7a0fad
Dinalba Navas <dinalba.navas@correo.unimet.edu.ve>
2021-11-19 17:17:52 -0400

    Reparado create

--- frontend/src/actions/foromessageAction.js
+++ frontend/src/actions/foromessageAction.js
@@ -1,33 +1,33 @@
 import {
-    FOROMESSAGE_LIST_REQUEST,
-    FOROMESSAGE_LIST_SUCCESS,
-    FOROMESSAGE_LIST_FAIL,
-    FOROMESSAGE_CREATE_REQUEST,
-    FOROMESSAGE_CREATE_SUCCESS,
-    FOROMESSAGE_CREATE_FAIL
-  } from "../constants/foromessageConstants";
-  import axios from "axios";
+  FOROMESSAGE_LIST_REQUEST,
+  FOROMESSAGE_LIST_SUCCESS,
+  FOROMESSAGE_LIST_FAIL,
+  FOROMESSAGE_CREATE_REQUEST,
+  FOROMESSAGE_CREATE_SUCCESS,
+  FOROMESSAGE_CREATE_FAIL,
+} from "../constants/foromessageConstants";
+import axios from "axios";
 
-  export const listForomessages = () => async (dispatch) => {
-    try {
-      dispatch({ type: FOROMESSAGE_LIST_REQUEST });
-  
-      const { data } = await axios.get("/api/foromessages");
-  
-      dispatch({ type: FOROMESSAGE_LIST_SUCCESS, payload: data });
-      localStorage.setItem("foromessages", JSON.stringify(data));
-    } catch (error) {
-      dispatch({
-        type: FOROMESSAGE_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+export const listForomessages = () => async (dispatch) => {
+  try {
+    dispatch({ type: FOROMESSAGE_LIST_REQUEST });
 
-  export const createForoMessage =
+    const { data } = await axios.get("/api/foromessages");
+
+    dispatch({ type: FOROMESSAGE_LIST_SUCCESS, payload: data });
+    localStorage.setItem("foromessages", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: FOROMESSAGE_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const createForoMessage =
   (username, message, icon) => async (dispatch, getState) => {
     try {
       dispatch({
